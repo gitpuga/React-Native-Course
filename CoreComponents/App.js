bf0b44c065e259ad679274d54b6c87ed129030d6
Svyatozar Naumov <svyatozar1@gmail.com>
2024-03-12 00:18:29 +0300

    Updated App.js

--- CoreComponents/App.js
+++ CoreComponents/App.js
@@ -1,31 +1,24 @@
-import { View, Text, VirtualizedList, Image, ImageBackground, ScrollView, Button, Pressable } from 'react-native';
+import { useState } from 'react';
+import { View, Text, VirtualizedList, Image, ImageBackground, ScrollView, Button, Pressable, Modal, StatusBar } from 'react-native';
 const logoImg = require("./assets/adaptive-icon.png");
 
 export default function App() {
+  const [isStatusBarHidden, setIsStatusBarHidden] = useState(false);
   return (
     <View style={{ flex: 1, backgroundColor: "white", padding: 60 }}>
-      <Button 
-        title='Press' 
-        onPress={() => console.log("Button Pressed")}
-        color="midnightblue"
+      <StatusBar 
+        backgroundColor="white"
+        barStyle="dark-content"
+        hidden={isStatusBarHidden}
       />
-      <Pressable onPress={() => console.log("Image Pressed")}>
-        <Image source={logoImg} style={{ width: 300, height: 300 }}/>
-      </Pressable>
-      <Pressable onPress={() => console.log("Text Pressed")}>
-        <Text>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-          condimentum, nisl eu ultricies, nunc nisl aliquam nunc, eget
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-          condimentum, nisl eu ultricies, nunc nisl aliquam nunc, eget
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-          condimentum, nisl eu ultricies, nunc nisl aliquam nunc, eget
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-          condimentum, nisl eu ultricies, nunc nisl aliquam nunc, eget
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-          condimentum, nisl eu ultricies, nunc nisl aliquam nunc, eget
-        </Text>
-      </Pressable>
+      <Button 
+        title="Status bar toggle" 
+        color="black"
+        onPress={
+          () => (isStatusBarHidden == false) ? 
+          setIsStatusBarHidden(true) : setIsStatusBarHidden(false)
+        }
+      />
     </View>
   );
 }  
