27475b5a292e66952291debc4c130add6acc3ca2
Svyatozar Naumov <svyatozar1@gmail.com>
2024-03-12 00:29:14 +0300

    Updated App.js

--- CoreComponents/App.js
+++ CoreComponents/App.js
@@ -1,23 +1,19 @@
 import { useState } from 'react';
-import { View, Text, VirtualizedList, Image, ImageBackground, ScrollView, Button, Pressable, Modal, StatusBar } from 'react-native';
+import { View, Text, VirtualizedList, Image, ImageBackground, ScrollView, Button, Pressable, Modal, StatusBar, ActivityIndicator } from 'react-native';
 const logoImg = require("./assets/adaptive-icon.png");
 
 export default function App() {
   const [isStatusBarHidden, setIsStatusBarHidden] = useState(false);
   return (
     <View style={{ flex: 1, backgroundColor: "white", padding: 60 }}>
-      <StatusBar 
-        backgroundColor="white"
-        barStyle="dark-content"
-        hidden={isStatusBarHidden}
-      />
-      <Button 
-        title="Status bar toggle" 
+      <ActivityIndicator 
         color="black"
-        onPress={
-          () => (isStatusBarHidden == false) ? 
-          setIsStatusBarHidden(true) : setIsStatusBarHidden(false)
-        }
+        size="large"
+      />
+      <ActivityIndicator 
+        color="green"
+        size="small"
+        animating={false}
       />
     </View>
   );
