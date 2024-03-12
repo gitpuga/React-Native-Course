84d8b125e1ee9bd72874dd6975f1e32a79d3ef4b
Svyatozar Naumov <svyatozar1@gmail.com>
2024-03-12 04:36:47 +0300

    updated: ./Layout

--- Layout/App.js
+++ Layout/App.js
@@ -1,20 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Box from './Components/Box';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Box style={{ backgroundColor: "red" }}>Box 1</Box>
+      <Box style={{ backgroundColor: "orange" }}>Box 2</Box>
+      <Box style={{ backgroundColor: "brown" }}>Box 3</Box>
+      <Box style={{ backgroundColor: "green" }}>Box 4</Box>
+      <Box style={{ backgroundColor: "midnightblue" }}>Box 5</Box>
+      <Box style={{ backgroundColor: "blue" }}>Box 6</Box>
+      <Box style={{ backgroundColor: "purple" }}>Box 7</Box>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 64,
+    borderColor: "red",
+    borderWidth: 6,
   },
 });
