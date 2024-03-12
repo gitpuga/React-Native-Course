8c965bbc9bfa9748be746a694295bf25991c47f0
Svyatozar Naumov <svyatozar1@gmail.com>
2024-03-12 17:28:45 +0300

    updated: ./DynamicUI/App.js

--- DynamicUI/App.js
+++ DynamicUI/App.js
@@ -1,53 +1,34 @@
-import { useState, useEffect } from 'react';
-import { StyleSheet, View, Text, Dimensions } from 'react-native';
+import { StyleSheet, View, Text, SafeAreaView } from 'react-native';
 
 export default function App() {
-
-  const [dimensions, setDimensions] = useState({
-    window: Dimensions.get("window")
-  });
-
-  useEffect(() => {
-    const subscribtion = Dimensions.addEventListener("change", ({ window }) => {
-      setDimensions({ window })
-    })
-    return () => subscribtion?.remove();
-  });
-
-  const { window } = dimensions
-  const windowWidth = window.width;
-  const windowHeight = window.height;
-
   return (
-    <View style={styles.container}>
-      <View style={[styles.box, {
-        width: windowWidth > 500 ? "70%" : "90%",
-        height: windowHeight > 600 ? "60%" : "90%",
-      }]}>
-        <Text style={{ fontSize: windowWidth > 500 ? 50 : 24 }}>Welcome!</Text>
+    <SafeAreaView style={styles.safeContainer}>
+      <View style={styles.container}>
+        <View style={styles.box}>
+          <Text style={styles.text}>Welcome!</Text>
+        </View>
       </View>
-    </View>
+    </SafeAreaView>
   );
 }
 
-// const windowWidth = Dimensions.get("window").width;
-// const windowHeight = Dimensions.get("window").height;
-
-// console.log({ windowWidth, windowHeight })
-
 const styles = StyleSheet.create({
+  safeContainer: {
+    flex: 1,
+    backgroundColor: "plum",
+    paddingTop: 30,
+    paddingBottom: 30,
+  },
   container: {
     flex: 1,
     backgroundColor: "plum",
-    alignItems: 'center',
-    justifyContent: 'center',
   },
   box: {
-    backgroundColor: "lightblue",
-    alignItems: "center",
-    justifyContent: "center",
+    padding: 20,
   },
-  // text: {
-  //   fontSize: windowWidth > 500 ? 50 : 24,
-  // },
+  text: {
+    fontSize: 24,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
 });
