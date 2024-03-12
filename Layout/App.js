b642bff340c4709358a8f55e32019c77859b1025
Svyatozar Naumov <svyatozar1@gmail.com>
2024-03-12 04:54:58 +0300

    updated: ./Layout/App.js

--- Layout/App.js
+++ Layout/App.js
@@ -5,11 +5,11 @@ import Box from './Components/Box';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Box style={{ backgroundColor: "red" }}>Box 1</Box>
-      <Box style={{ backgroundColor: "orange" }}>Box 2</Box>
-      <Box style={{ backgroundColor: "brown" }}>Box 3</Box>
-      <Box style={{ backgroundColor: "green" }}>Box 4</Box>
-      <Box style={{ backgroundColor: "midnightblue" }}>Box 5</Box>
+      <Box style={{ backgroundColor: "red", alignSelf: "flex-start"}}>Box 1</Box>
+      <Box style={{ backgroundColor: "orange", alignSelf: "flex-end"}}>Box 2</Box>
+      <Box style={{ backgroundColor: "brown", alignSelf: "center" }}>Box 3</Box>
+      <Box style={{ backgroundColor: "green", alignSelf: "stretch" }}>Box 4</Box>
+      <Box style={{ backgroundColor: "midnightblue", alignSelf: "auto" }}>Box 5</Box>
       <Box style={{ backgroundColor: "blue" }}>Box 6</Box>
       <Box style={{ backgroundColor: "purple" }}>Box 7</Box>
     </View>
@@ -18,6 +18,8 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+    alignItems: "flex-end",
     marginTop: 64,
     borderColor: "red",
     borderWidth: 6,
