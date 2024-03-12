cca6e495dd2d7e29c886cfd41e2bb45eb31347a7
Svyatozar Naumov <svyatozar1@gmail.com>
2024-03-12 23:57:01 +0300

    updated: ./Forms/App.js

--- Forms/App.js
+++ Forms/App.js
@@ -1,33 +1,49 @@
-import { useState } from 'react';
 import { 
-  StyleSheet, 
-  Text, 
-  View, 
-  StatusBar, 
-  SafeAreaView,
+  View,
+  Text,
   TextInput,
+  Button,
+  StyleSheet,
+  StatusBar,
+  SafeAreaView,
+  Platform
 } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
-  const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <SafeAreaView style={styles.container}>
-      <TextInput 
-        style={styles.input}
-        value={name}
-        onChangeText={setName}
-        placeholder="email@example.com"
-        autoCorrect={false}
-        autoCapitalize="none"
-        // secureTextEntry
-        // keyboardType=""
-      />
-      <TextInput 
-        style={[styles.input, styles.multilineText]}
-        placeholder="message"
-        multiline
-      />
-      <Text style={styles.text}>My name is {name}</Text>
+      <View style={styles.form}>
+        <Text style={styles.label}>Username</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Enter your username"
+          value={username}
+          onChangeText={setUsername}
+        />
+        <Text style={styles.label}>Password</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Enter your password"
+          value={password}
+          onChangeText={setPassword}
+          secureTextEntry
+        />
+
+        <View style={styles.buttonContainer}>
+          <Button
+            title="Sign In" 
+            onPress={() => {}} 
+          />
+          <Button
+            title="Sign Up" 
+            onPress={() => {}} 
+          />
+        </View>
+      </View>
     </SafeAreaView>
   );
 }
@@ -35,24 +51,39 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: "center",
+    paddingHorizontal: 20,
     backgroundColor: '#f5f5f5',
     paddingTop: StatusBar.currentHeight
   },
+  form: {
+    backgroundColor: "white",
+    padding: 20,
+    borderRadius: 10,
+    shadowColor: "black",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
+  },
+  label: {
+    fontSize: 16,
+    marginBotton: 5,
+    fontWeight: "bold"
+  },
   input: {
     height: 40,
-    margin: 12,
-    padding: 10,
+    borderColor: "#ddd",
     borderWidth: 1,
-    borderRadius: 10
-  },
-  text: {
-    fontSize: 30,
-    fontWeight: "bold",
+    marginBottom: 15,
     padding: 10,
-    textAlign: "center"
+    borderRadius: 5
   },
-  multilineText: {
-    minHeight: 100,
-    textAlignVertical: "top"
-  }
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between"
+  },
 });
